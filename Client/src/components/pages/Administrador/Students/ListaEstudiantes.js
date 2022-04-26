e0619b6f75d04fa6aaee42c851df1038825e2c6d
JuanC001 <57727114+JuanC001@users.noreply.github.com>
2022-04-26 14:56:23 -0500

    ListaEst
    Pequeños graficos cambiados

--- Client/src/components/pages/Administrador/Students/ListaEstudiantes.js
+++ Client/src/components/pages/Administrador/Students/ListaEstudiantes.js
@@ -70,6 +70,9 @@ export default function ListaEstudiantes(props) {
           <div className="col-sm-2">
             <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" className="img-fluid rounded-start border" />
           </div>
+          <div className="col-sm-10">
+            
+          </div>
         </div>
 
       </div>
