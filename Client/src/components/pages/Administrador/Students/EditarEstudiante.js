8a21e973f85e5dfc194e616c2317b2edf0016290
JuanC001 <57727114+JuanC001@users.noreply.github.com>
2022-04-27 12:36:22 -0500

    Update EditarEstudiante.js

--- Client/src/components/pages/Administrador/Students/EditarEstudiante.js
+++ Client/src/components/pages/Administrador/Students/EditarEstudiante.js
@@ -160,13 +160,7 @@ class EditarEstudiante extends React.Component {
                                 <Accordion.Item eventKey="1">
                                     <Accordion.Header>Rotacion 2: {this.state.rotacion2.lugar}</Accordion.Header>
                                     <Accordion.Body>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                        est laborum.
+                                        
                                     </Accordion.Body>
                                 </Accordion.Item>
                             </Accordion>
