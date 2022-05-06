491f531a74d0e89fdc86f30e1f440f3344a53360
JuanC001 <57727114+JuanC001@users.noreply.github.com>
2022-05-05 19:18:27 -0500

    Estudiantes
    + Estudiantes de doctores

--- Client/src/components/pages/Doctores/Estudiantes.js
+++ Client/src/components/pages/Doctores/Estudiantes.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react'
 export default class Estudiantes extends Component {
   render() {
     return (
-      <div>Estudiantes</div>
+      <div>
+
+        <h1 className="display-1">
+            Estudiantes inscritos a: 
+        </h1>
+
+      </div>
     )
   }
 }
